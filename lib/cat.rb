a129d55a577421b97a87c6450750cbec16c98d58
Samuel Cornet <samuel@noukod.com>
2018-10-05 18:53:13 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,14 @@
 class Cat
-  attr_accessor :name
+  attr_accessor :mood
+  attr_reader :name
 
   def initialize(name)
     @name = name
+    @mood = "nervous"
   end
+
+  # def self.mood
+  #  @name = self.name
+  # end
+
 end
