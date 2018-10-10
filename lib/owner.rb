5da6cdf54340afb8f71072a98dc1429d16699738
Merline Belizaire <merline@noukod.com>
2018-10-10 17:44:54 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
+#require "pry"
 class Owner
   # code goes here
+  attr_reader :species
+    attr_accessor :name,:pets
+  
+    @@all=[]
+  
+    def initialize(name)
+      @name=name
+      @@all << self
+      @species="human"
+      @pets = {:fishes:=>[],:dogs=>[],:cats=>[]}
+    end
+  
+    def self.count
+      @@all.count
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def self.reset_all
+      @@all=[]
+    end
 end
