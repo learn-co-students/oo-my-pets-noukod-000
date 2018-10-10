dfeff4338ef0c8f74e99a739c652d03c559c11e6
Merline Belizaire <merline@noukod.com>
2018-10-10 17:47:50 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,34 +1,8 @@
 #require "pry"
 class Owner
   # code goes here
-  attr_reader :species
-    attr_accessor :name,:pets
+  #attr_reader :species
+  #attr_accessor :name,:pets
 
-    @@all=[]
-
-    def initialize(name)
-      @name=name
-      @@all << self
-      @species="human"
-      @pets = {:fishes:=>[],:dogs=>[],:cats=>[]}
-    end
-
-    def self.count
-      @@all.count
-    end
-
-    def self.all
-      @@all
-    end
-
-    def self.reset_all
-      @@all=[]
-    end
-    def say_species
-    "I am a #{species}."
-  end
-
-  def list_pets
-    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
-  end
+  @@all=[]
 end
