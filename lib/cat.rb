b09215b5fce1a1a05490d86234b0c2390054ff49
Ricardo Paul <kingupper0@gmail.com>
2019-04-26 15:36:28 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
+  attr_accessor :mood
+  attr_reader :name
   # code goes here
+  def initialize(name, mood = nil)
+    @mood = mood
+    @name = name
+    self.mood = "nervous"
+  end
+  
+  
+  
 end
