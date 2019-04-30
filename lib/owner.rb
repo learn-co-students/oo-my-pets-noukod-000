90c732390de959576d75d867678f82d0aaabd8d0
Gilbert Torchon <gilbert@noukod.com>
2019-04-30 14:42:43 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,76 +2,71 @@ require 'pry'
 
 class Owner
   # code goes here
-
+  attr_accessor :pets, :name
   attr_reader :species
-  attr_accessor :name,:pets
 
-  @@all=[]
+   @@all = []
 
-  def initialize(name)
-    @name=name
+   def initialize(species)
+    @species = species
     @@all << self
-    @species="human"
-    @pets = {:fishes:=>[],:dogs=>[],:cats=>[]}
+    @pets = { :fishes => [], :dogs => [], :cats => [] }
   end
 
-  def self.count
-    @@all.count
-  end
-
-  def self.all
+   def self.all
     @@all
   end
 
-  def self.reset_all
-    @@all=[]
+   def self.reset_all
+    @@all.clear
   end
 
-  def say_species
-    "I am a #{@species}."
+   def self.count
+    @@all.size
   end
 
-  def buy_fish(fish_name)
-    fish = Fish.new(fish_name)
-    @pets[:fishes] << fish
+   def say_species
+    "I am a #{species}."
   end
 
-  def buy_dog(dog_name)
-    dog = Dog.new(dog_name)
-    @pets[:dogs] << dog
-    #binding.pry
+   def buy_fish(name)
+    fish = Fish.new(name)
+    pets[:fishes] << fish
   end
 
-  def buy_cat(cat_name)
-    cat = Cat.new(cat_name)
-    @pets[:cats] << cat
+   def buy_cat(name)
+    cat = Cat.new(name)
+    pets[:cats] << cat
   end
 
-  def walk_dogs
-    @pets[:dogs].collect do |dog|
-      dog.mood="happy"
+   def buy_dog(name)
+    dog = Dog.new(name)
+    pets[:dogs] << dog
+  end
+
+   def walk_dogs
+    pets[:dogs].map {|dog| dog.mood = "happy"}
+
+   end
+
+   def play_with_cats
+    pets[:cats].map {|cat| cat.mood = "happy"}
+  end
+
+   def feed_fish
+    pets[:fishes].map {|fish| fish.mood = "happy"}
+  end
+
+   def sell_pets
+    pets.each do |type, pets|
+      pets.map {|pet| pet.mood = "nervous"}
     end
+    pets.clear
   end
 
-  def play_with_cats
-    @pets[:cats].collect do |cat|
-      cat.mood="happy"
-    end
+   def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
 
-  def feed_fish
-    @pets[:fishes].collect do |fish|
-      fish.mood="happy"
-    end
-  end
-
-  def sell_pets
-    @pets.each do |pet_collection|
-      pet_collection.collect do |pet|
-        binding.pry
-        pet.mood=("nervous")
-      end
-    end
-  end
 
 end
